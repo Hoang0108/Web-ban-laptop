2528fb7e7bdaa065bd9d990041bd2a88529b120a
Nguyen Thai Hoang <n.t.hoang1821@gmail.com>
2023-06-13 00:43:48 +0700

    chua co responsive nen danh cho may co resolution 1920x1080

---
+++ Js/slide.js
@@ -0,0 +1,41 @@
+var slideIndex = 1;
+showSlide(slideIndex);
+autoSlide(); 
+
+function changeSlide(n) {
+  showSlide((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlide((slideIndex = n));
+}
+
+function showSlide(n) {
+  var slides = document.getElementsByClassName("slide");
+  var dots = document.getElementsByClassName("dot");
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
+  for (var i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  for (var i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active-dot", "");
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active-dot";
+}
+
+function autoSlide() {
+  setInterval(function () {
+    changeSlide(1);
+  }, 2000); 
+}
